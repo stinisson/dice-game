843eb4ec85fd59d2a91031bb10c52e5c5cfc7796
stin <krikarlss@gmail.com>
2021-09-09 16:59:43 +0200

    Start on roll and translation routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import random
 
 app = Flask(__name__)
 
@@ -7,6 +8,27 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
+
+
+@app.route('/magic-rule')
+def translate_roll(roll):
+    # Take roll as a list and calculate the total number of dots according to the magic rule
+    # roll = [1, 2, 3, 4, 5, 6]
+
+    dotSum = 0
+    for dice in roll:
+        if dice == 5:
+            dotSum += 4
+        elif dice == 3:
+            dotSum += 2
+    return dotSum
+
+
+@app.route('/roll')
+def generate_roll():
+    roll = [random.randint(1, 6) for i in range(6)]
+    translation = translate_roll(roll)
+    return {"roll": roll, "translation": translation}
 
 
 @app.route('/main.png')

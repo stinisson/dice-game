a0117bcbdffd33f122b24538aa3c32756bebe00f
stin <krikarlss@gmail.com>
2021-09-09 14:37:36 +0200

    Change colours of button elements

--- static/dice.js
+++ static/dice.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     $('#gameProgress').hide();
     $('#gameControl').hide();
     $('#gameHelp').hide();
+
 });
 
 function startGame() {
@@ -30,9 +31,17 @@ function startGame() {
     $('#gameProgressBar').removeClass("bg-success");
     $('#gameProgressBar').addClass("bg-info");
     $("#startGame").hide();
+    $("#gameHint").hide();
+    $("#gameNonSuccess").hide();
 
     //$("#numDotsHistory").empty();
     //$("#throwHistory").empty();
+
+    $("#numDotsHistory").empty();
+    $("#throwHistory").empty();
+    $("#guessHistory").empty();
+
+    $('#dotGuess').css("border-color", "#ced4da");
 
 }
 
@@ -43,6 +52,7 @@ function restartGame() {
     $("#guessHistory").empty();
 }
 
+
 $( "#startGame" ).click(function() {
   startGame();
 });
@@ -52,45 +62,56 @@ $( "#makeGuess" ).click(function() {
     const answer = 42;
     let newProgress;
 
-    let guess = $("#dotGuess").val();
-    if (parseInt(guess)) {
+    let guess = parseInt($("#dotGuess").val());
+
+    if (!isNaN(guess)) {
+
+       $('#numDotsHistory').append("777"  + "<br>");
+       $('#throwHistory').append("777"  + "<br>");
+       $('#guessHistory').append(guess + "<br>");
+
         if (parseInt(guess) === answer) {
            let currentProgress = $('#gameProgressBar').attr('aria-valuenow');
            newProgress = parseInt(currentProgress) + 25;
            $('#gameProgressBar').attr('aria-valuenow', newProgress).css('width', newProgress+'%');
+
+           $('#dotGuess').css("border-color", "#7ED957");
+
            if (newProgress === 100) {
               $('#gameProgressBar').removeClass("bg-info");
               $('#gameProgressBar').addClass("bg-success");
               $('#gameSuccess').show();
-
               $("#startGame").text("Play again");
               $("#startGame").show();
-              restartGame();
+              //restartGame();
            }
-
         } else {
-            alert( "You guessed: " + guess + ". That was not correct. Try again!" );
+            console.log( "You guessed: " + guess + ". That was not correct. Try again!" );
             newProgress = 0;
             $('#gameProgressBar').attr('aria-valuenow', newProgress).css('width', newProgress+'%');
             $('#gameProgressBar').removeClass("bg-success");
             $('#gameProgressBar').addClass("bg-info");
+            $('#dotGuess').css("border-color", "#FF5757");
         }
     } else {
         alert( "Submit an integer in the range (-∞, ∞)" );
+        $('#dotGuess').css("border-color", "#FF5757")
+
     }
-
     $( "#dotGuess" ).val('');
-
 });
 
+// Show hint
 $( "#showHint" ).click(function() {
     $("#gameHint").show();
 });
 
+// Show solution
 $( "#showSolution" ).click(function() {
     $("#gameNonSuccess").show();
 });
 
+// Show game instructions
 $( "#showInstructions" ).click(function() {
     $("#gameInstructions").show();
     $('#gameInstructions').alert();

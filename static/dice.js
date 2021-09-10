5c0b25940d437751abdb002c9c4eb14c3790df6e
stin <krikarlss@gmail.com>
2021-09-10 12:24:02 +0200

    Disable input field on restart

--- static/dice.js
+++ static/dice.js
@@ -4,53 +4,60 @@
 // update line colour on text input, disable guess button + change dice image
 
 $(document).ready(function() {
-    $('#diceThrows').hide();
-    $('#gameInfo').hide();
-    $('#gameHistory').hide();
-    $('#gameGuess').hide();
-    $('#gameSuccess').hide();
-    $('#gameHint').hide();
-    $('#gameNonSuccess').hide();
-    $('#gameProgress').hide();
-    $('#gameControl').hide();
-    $('#gameHelp').hide();
-
+    const elements = ['#diceRolls', '#gameInfo', '#gameHistory', '#gameGuess', '#gameSuccess', '#gameHint',
+        '#gameNonSuccess', '#gameProgress', '#gameControl', '#gameHelp'];
+    elements.forEach(element => $(element).hide());
 });
 
 function startGame() {
-    $("#startImage").hide();
-    $('#diceThrows').show();
-    $("#gameInfo").show();
-    $('#gameHistory').show();
-    $('#gameGuess').show();
-    $("#gameProgress").show();
-    $("#gameControl").show();
-    $("#gameHelp").show();
-    $('#gameSuccess').hide();
+    gameSetup();
+    enableGameControl();
+    emptyHistory();
+    roll();
+}
+
+function gameSetup() {
+    const showOnStart = ['#diceRolls', '#gameInfo', '#gameHistory', '#gameGuess', '#gameProgress', '#gameControl',
+        '#gameHelp', '#gameProgress', '#gameControl', '#gameHelp'];
+    const hideOnStart = ['#startImage', '#gameSuccess', '#startGame', '#gameHint', '#gameNonSuccess'];
+    showOnStart.forEach(element => $(element).show());
+    hideOnStart.forEach(element => $(element).hide());
+
     $('#gameProgressBar').attr('aria-valuenow', 0).css('width', 0+'%');
     $('#gameProgressBar').removeClass("bg-success");
     $('#gameProgressBar').addClass("bg-info");
-    $("#startGame").hide();
-    $("#gameHint").hide();
-    $("#gameNonSuccess").hide();
-
-    //$("#numDotsHistory").empty();
-    //$("#throwHistory").empty();
-
-    $("#numDotsHistory").empty();
-    $("#throwHistory").empty();
-    $("#guessHistory").empty();
-
     $('#dotGuess').css("border-color", "#ced4da");
-
 }
 
-// remove this after test run, should only be in startGame()
-function restartGame() {
-    $("#numDotsHistory").empty();
-    $("#throwHistory").empty();
-    $("#guessHistory").empty();
+
+function emptyHistory() {
+    const elementsToReset = ['#numDotsHistory', '#rollHistory', '#guessHistory'];
+    elementsToReset.forEach(element => $(element).empty());
 }
+
+function roll() {
+    $.get( "/roll", function(data) {})
+    .done(function(data) {
+        let rollHistory = "| "
+        data.roll.forEach(dice => rollHistory += dice + " | ");
+        $('#rollHistory').append(rollHistory + "<br>");
+        $('#numDotsHistory').append(data.translation + "<br>");
+    })
+    .fail(function(data) {
+        console.log(data)
+        alert( "Error while contacting the server");
+    });
+}
+
+$( "#rollDice" ).click(function() {
+    if ($(this).hasClass('disabled')) {
+        return false;
+    }
+    $('#rollDice').addClass('disabled');
+    roll();
+    $('#makeGuess').removeClass('disabled');
+    $('#makeGuess').addClass('enabled');
+});
 
 
 $( "#startGame" ).click(function() {
@@ -59,6 +66,11 @@ $( "#startGame" ).click(function() {
 
 $( "#makeGuess" ).click(function() {
 
+    // Allow only one guess per roll
+    if ($(this).hasClass('disabled')) {
+        return false;
+    }
+
     const answer = 42;
     let newProgress;
 
@@ -66,15 +78,17 @@ $( "#makeGuess" ).click(function() {
 
     if (!isNaN(guess)) {
 
-       $('#numDotsHistory').append("777"  + "<br>");
-       $('#throwHistory').append("777"  + "<br>");
-       $('#guessHistory').append(guess + "<br>");
+        // Disable button after valid guess
+        $(this).addClass('disabled');
 
+        $('#rollDice').removeClass('disabled');
+        $('#rollDice').addClass('enabled');
+
+        $('#guessHistory').append(guess + "<br>");
         if (parseInt(guess) === answer) {
            let currentProgress = $('#gameProgressBar').attr('aria-valuenow');
            newProgress = parseInt(currentProgress) + 25;
            $('#gameProgressBar').attr('aria-valuenow', newProgress).css('width', newProgress+'%');
-
            $('#dotGuess').css("border-color", "#7ED957");
 
            if (newProgress === 100) {
@@ -83,7 +97,6 @@ $( "#makeGuess" ).click(function() {
               $('#gameSuccess').show();
               $("#startGame").text("Play again");
               $("#startGame").show();
-              //restartGame();
            }
         } else {
             console.log( "You guessed: " + guess + ". That was not correct. Try again!" );
@@ -93,26 +106,84 @@ $( "#makeGuess" ).click(function() {
             $('#gameProgressBar').addClass("bg-info");
             $('#dotGuess').css("border-color", "#FF5757");
         }
+
     } else {
         alert( "Submit an integer in the range (-∞, ∞)" );
         $('#dotGuess').css("border-color", "#FF5757")
-
     }
     $( "#dotGuess" ).val('');
+
 });
+
+function enableGameControl() {
+    $('#dotGuess').prop('readonly', false);
+
+    $('#makeGuess').addClass('enabled');
+    $('#makeGuess').removeClass('disabled');
+
+    $('#rollDice').addClass('enabled');
+    $('#rollDice').removeClass('disabled');
+
+    $('#help').addClass('enabled');
+    $('#help').removeClass('disabled');
+}
+
+function disableGameControl() {
+    $('#dotGuess').prop('readonly', true);
+
+    $('#makeGuess').addClass('disabled');
+    $('#makeGuess').removeClass('enabled');
+
+    $('#rollDice').addClass('disabled');
+    $('#rollDice').removeClass('enabled');
+
+    $('#help').addClass('disabled');
+    $('#help').removeClass('enabled');
+}
+
+function endGame() {
+    gameSetup();
+    $("#startGame").show();
+    $("#startGame").text('Play again');
+    disableGameControl();
+}
 
 // Show hint
 $( "#showHint" ).click(function() {
     $("#gameHint").show();
 });
 
+// Close hint
+$('#closeHint').click(function() {
+   $('#gameHint').hide();
+})
+
 // Show solution
 $( "#showSolution" ).click(function() {
     $("#gameNonSuccess").show();
 });
 
+// Close solution
+$('#closeSolution').click(function() {
+   $('#gameNonSuccess').hide();
+})
+
 // Show game instructions
 $( "#showInstructions" ).click(function() {
     $("#gameInstructions").show();
-    $('#gameInstructions').alert();
 });
+
+// End game
+$( "#endGame" ).click(function() {
+    endGame();
+});
+
+// Close instructions
+$('#closeInstructions').click(function() {
+   $('#gameInstructions').hide();
+})
+
+// Close game success
+$('#closeGameSuccess').click(function() {
+   $('#gameSuccess').hide();
+})
